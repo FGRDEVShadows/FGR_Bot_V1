b96118afe103f398ec4d75294f0645d74e56db3d
FGRDEVShadows <fgrdevshadows@gmail.com>
2024-07-20 01:41:51 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,48 +1,5 @@
 wait(1)
 
--- script 1
+-- bot
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/FGRDEVShadows/FGR_Bot_V1/main/script3.lua"))()
-
-wait(1)
-
--- script 2
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/FGRDEVShadows/FGR_Bot_V1/main/script4.lua"))()
-
-wait(1)
-
--- script 3
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/FGRDEVShadows/FGR_Bot_V1/main/script5.lua"))()
-
-wait(1)
-
--- script 4
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/FGRDEVShadows/FGR_Bot_V1/main/script6.lua"))()
-
-wait(2)
-
--- script 5
-
-loadstring(game:HttpGet(""))()
-
-wait(2)
-
---script 6
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/FGRDEVShadows/FGR_Bot_V1/main/script8.lua"))()
-
-wait(2)
-
---script 7
-
-loadstring(game:HttpGet(""))()
-
-wait(2)
-
---script 8
-
-loadstring(game:HttpGet(""))()
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/FGRDEVShadows/FGR_Bot_V1/main/bot.lua"))()
