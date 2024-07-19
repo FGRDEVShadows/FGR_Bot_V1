c2a03165f7c83a41479bf141aa9c9ff57ac50f82
FGRDEVShadows <fgrdevshadows@gmail.com>
2024-07-19 18:00:45 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -10,21 +10,31 @@ _G.Config = {
 
 wait(1)
 
+-- script 1
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/FGRDEVShadows/FGR_Bot_V1/main/script3.lua"))()
 
 wait(1)
+
+-- script 2
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/FGRDEVShadows/FGR_Bot_V1/main/script4.lua"))()
 
 wait(1)
 
+-- script 3
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/FGRDEVShadows/FGR_Bot_V1/main/script5.lua"))()
 
 wait(1)
 
+-- script 4
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/FGRDEVShadows/FGR_Bot_V1/main/script6.lua"))()
 
 wait(2)
+
+-- script 5
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/FGRDEVShadows/FGR_Bot_V1/main/script7.lua"))()
 
